e65e664716d94c82fdb90628195df9a4ec90c426
Maciej Mazurek <157367207+MaciejMazurek-Dev@users.noreply.github.com>
2026-04-25 01:31:18 +0200

    Add extended boot record

--- src/bootloader.asm
+++ src/bootloader.asm
@@ -5,78 +5,78 @@
 ;
 ;**************************************
 
+bits 16										; Generate code to run on a processor operating in 16-bit mode
+org 0x7c00									; Load this code into memory starting from address 0x7C00
 
 
-bits 16									; Generate code to run on a processor operating in 16-bit mode
-org 0x7c00								; Load this code into memory starting from address 0x7C00
+jmp main									; Jump over BPB
+nop											; 
+
+;****************************************
+;	BIOS PARAMETER BLOCK  
+;****************************************
+											; Offset
+bpb_oem_identifier db "MSWIN4.1"			; 0x03	
+bpb_bytes_per_sector dw 512					; 0x0b
+bpb_sectors_per_cluster db 1				; 0x0d
+bpb_reserved_sectors dw 1					; 0x0e
+bpb_file_allocation_tables db 2				; 0x10
+bpb_root_directory_entries dw 512			; 0x11 - How many files/folders we can create in root directory
+bpb_total_sectors dw 0						; 0x13
+bpb_media_descriptor db 0xf8				; 0x15 - Hard drive
+bpb_sectors_per_fat dw 18					; 0x16
+bpb_sectors_per_track dw 32					; 0x18
+bpb_heads_per_cylinder dw					; 0x1a
+bpb_hidden_sectors dw 0						; 0x1c
+bpb_large_sector dd 131070					; 0x20
 
 
-jmp main								; Jump over BPB
-nop										; 
+;*****************************************
+;  EXTENDED BOOT RECORD  
+;*****************************************
 
-;*************************
-;*	BIOS PARAMETER BLOCK *
-;*************************
-										; Offset
-oem_identifier db "MSWIN4.1"			; 0x03	
-bytes_per_sector dw 512					; 0x0b
-sectors_per_cluster db 1				; 0x0d
-reserved_sectors dw 1					; 0x0e
-file_allocation_tables db 2				; 0x10
-root_directory_entries dw 512			; 0x11 - How many files/folders we can create in root directory
-total_sectors dw 0						; 0x13
-media_descriptor db 0xf8				; 0x15 - Hard drive
-sectors_per_fat dw 18					; 0x16
-sectors_per_track dw 32					; 0x18
-heads_per_cylinder dw					; 0x1a
-hidden_sectors dw 0						; 0x1c
-large_sector dd 131070					; 0x20
-
-;**************************
-;*  EXTENDED BOOT RECORD  *
-;**************************
-
-drive_number db 0						; 0x24
-flags db 0								; 0x25
-extended_boot_signature db 0x29			; 0x26
+ebpb_drive_number db 0						; 0x24
+ebpb_flags db 0								; 0x25
+ebpb_extended_boot_signature db 0x29		; 0x26
+ebpb_volume_id_serial_number dd 0			; 0x27
+ebpb_volume_label db "Hard disk  "			; 0x2b - Must be 11 characters
+ebpb_file_system_identifier db "FAT16   "	; 0x36 - Must be 8 characters
 
 
-message db "Bootloader - Stage 1"		; String to be displayed
-message_length equ ($ - message)		; Length of the string
+main:										; 0x3E - Bootloader code
+mov drive_number, dl						; Save current drive number which is passed by BIOS via DL register
 
-drive_number db 
-
-main:
-mov drive_number, dl					; Get current drive number
-
-display_mssage:
-		mov ah, 0x13					; Function 0x13
-		mov al, 0x01					; Move cursor after writing
-		mov bh, 0						; Page number
-		mov bl, 0x0f					; White color
-		mov cx, message_length			; String length
-		mov dh, [0x0d]					; Row
-		mov dl, 0x0a					; Column
-		mov bp, message					; String address
-		int 0x10						; 
+display_message:
+mov ah, 0x13								; Function 0x13
+mov al, 0x01								; Move cursor after writing
+mov bh, 0									; Page number
+mov bl, 0x0f								; White color
+mov cx, message_length						; String length
+mov dh, [0x0d]								; Row
+mov dl, 0x0a								; Column
+mov bp, message								; String address
+int 0x10									; 
 
 read_disk:
-mov ax, 0x1000							; Write data from disk into memory address 0x1000 (ES:BX)
+mov ax, 0x1000								; Write data from disk into memory address 0x1000 (ES:BX)
 mov es, ax
 xor bx, bx
 
 	.read
-	mov ah, 0x02						; Function 0x02
-	mov al, 0x01						; Number of sectors to read
-	mov ch, 0							; Cylinder number
-	mov cl,	0x02						; Sector number
-	mov dh, 0							; Head number
-	mov dl, [drive_number]				; Drive number
-	int 0x13							; Read data from disk
+	mov ah, 0x02							; Function 0x02
+	mov al, 0x01							; Number of sectors to read
+	mov ch, 0								; Cylinder number
+	mov cl,	0x02							; Sector number
+	mov dh, 0								; Head number
+	mov dl, [fat_drive_number]				; Drive number
+	int 0x13								; Read data from disk
 	jc	.read
 	jmp 0x1000:0	
 
 
-times (510 - ($ - $$)) db 0				; Pade with zeroes until we get 510 byte in size.
-db 0x55									; Boot signature (byte offset 510)
-db 0xaa									; Boot signature (byte offset 511)
+;-----------------------------------------------------------------------
+message db "Bootloader - Stage 1"			; String to be displayed
+message_length equ ($ - message)			; Length of the string
+
+times (510 - ($ - $$)) db 0					; Pade with zeroes until we get 510 byte in size.
+dw 0xaa55									; Boot signature 
